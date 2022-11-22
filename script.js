563307c6d256e81182d90f48b8f8adef2caf247a
Quyen Nguyen <quyenlearntocode@gmail.com>
2022-11-22 13:13:38 -0500

    updated design

--- script.js
+++ script.js
@@ -1,3 +1,113 @@
+// algebra calculator
+const numberBtns = document.querySelectorAll(`[data-number]`);
+const operatorBtns = document.querySelectorAll(`[data-operator]`);
+const equalBtn = document.querySelector(`[data-equal]`);
+const acBtn = document.querySelector(`[data-allclear]`);
+const delBtn = document.querySelector(`[data-delete]`);
+const output = document.querySelector(`[data-result]`);
+const previousOperand = document.querySelector("#previous-operand");
+const currentOperand = document.querySelector("#current-operand");
+
+class Calculator {
+  constructor(previousOperand, currentOperand) {
+    this.previousOperand = previousOperand;
+    this.currentOperand = currentOperand;
+    this.clear();
+  }  
+  appendNumber(number) {
+    if (number == "." && this.currentOperandTextElement.includes(".")) {
+      return;
+    }
+    this.currentOperandTextElement += number;
+    this.updateDisplay();
+  }
+  chooseOperation(operator) {
+    if (this.currentOperandTextElement == "" && this.previousOperandTextElement == "") {
+      return;
+    }
+    if (this.currentOperandTextElement == "" && this.previousOperandTextElement != "") {
+      this.operation = operator;
+      this.previousOperandTextElement = this.previousOperandTextElement.slice(0, -1) + operator;
+    }
+    if (this.currentOperandTextElement != "" && this.previousOperandTextElement != "") {
+      this.calculate();
+    }
+    if (this.currentOperandTextElement != "" && this.previousOperandTextElement == "") {
+      this.operation = operator;
+      this.previousOperandTextElement = this.currentOperandTextElement + " " + operator;
+      this.currentOperandTextElement = "";
+    }
+    this.updateDisplay();
+  }
+  calculate() {
+    let x = this.previousOperandTextElement.slice(0, -2);
+    let y = this.currentOperandTextElement;
+    if (x == "" || y == "") {
+      return;
+    }
+    x = Number(x);
+    y = Number(y);
+    switch(this.operation) {
+      case "+":
+        this.currentOperandTextElement = String(x + y);
+        break;
+      case "-":
+        this.currentOperandTextElement = String(x - y);
+        break;
+      case "*":
+        this.currentOperandTextElement = String(x * y);
+        break;
+      case "/":
+        this.currentOperandTextElement = String(x / y);
+        break;
+      default:
+        return;
+    }
+    this.previousOperandTextElement = "";
+    this.updateDisplay();
+  }
+  delete() {
+    this.currentOperandTextElement = this.currentOperandTextElement.slice(0, -1);
+    this.updateDisplay();
+  }
+  clear() {
+    this.previousOperandTextElement = "";
+    this.currentOperandTextElement = "";
+    this.operation = undefined;
+    this.updateDisplay();
+  }
+  updateDisplay() {
+    this.previousOperand.innerText = this.previousOperandTextElement;
+    this.currentOperand.innerText = this.currentOperandTextElement;
+  }
+}
+
+const calculator = new Calculator(previousOperand, currentOperand);
+
+numberBtns.forEach(btn => {
+  btn.onclick = () => {
+    calculator.appendNumber(btn.innerText);
+  }
+});
+
+operatorBtns.forEach(btn => {
+  btn.onclick = () => {
+    calculator.chooseOperation(btn.innerText);
+  }
+});
+
+equalBtn.onclick = () => {
+  calculator.calculate();
+};
+
+acBtn.onclick = () => {
+  calculator.clear();
+};
+
+delBtn.onclick = () => {
+  calculator.delete();
+};
+
 // tip calculator
 const totalInput = document.getElementById("total-input");
 const taxInput = document.getElementById("tax-input");
